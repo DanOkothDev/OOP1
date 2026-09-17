116c7beed103bcfffa2166e76c036c0fe890d0e4
CupidVybz <omondiokothdan@gmail.com>
2026-09-17 12:08:18 +0300

    OOP1

---
+++ ClassWork/Lesson1/Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by NexaLab on 9/17/2026.
+//
+
+#include "Main.h"
---
+++ ClassWork/Lesson1/Main.h
@@ -0,0 +1,13 @@
+//
+// Created by NexaLab on 9/17/2026.
+//
+
+#ifndef OOP1_MAIN_H
+#define OOP1_MAIN_H
+
+
+class Main {
+};
+
+
+#endif //OOP1_MAIN_H
